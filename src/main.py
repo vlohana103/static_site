381da16d9282343aa4a3a99276861fb02b045a3e
Vikas <vlohana103@gmail.com>
2026-03-27 01:22:57 -0400

    finished part 5 push to github

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from markdown_blocks import markdown_to_html_node, extract_title
 import shutil
 import os
+import sys
 
 from textnode import TextNode, TextType
 
 def main():
-   if os.path .exists("public"):
-      shutil.rmtree("public")
-   os.mkdir("public")
+   basepath = "/"
+   if len(sys.argv) > 1:
+      basepath = sys.argv[1]
+
+   if os.path.exists("docs"):
+      shutil.rmtree("docs")
+   os.mkdir("docs")
 
    dummy_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
    print(dummy_node)
-   recursive_function("static", "public")
+   recursive_function("static", "docs")
 
    # generate_page("content/index.md", "template.html", "public/index.html")
 
@@ -19,7 +24,7 @@ def main():
    # generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
    # generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
    # generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
-   generate_pages_recursive("content", "template.html", "public")
+   generate_pages_recursive("content", "template.html", "docs", basepath)
 
 def recursive_function(source, destination):
    if os.path.exists(destination):
@@ -35,7 +40,7 @@ def recursive_function(source, destination):
             os.mkdir(new_des)
             recursive_function(joined, new_des)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
 
    with open(from_path, "r") as f:
@@ -50,12 +55,15 @@ def generate_page(from_path, template_path, dest_path):
 
    full_html = template.replace("{{ Title }}", title).replace("{{ Content }}", content_html)
 
+   full_html = full_html.replace('href="/', f'href="{basepath}')
+   full_html = full_html.replace('src="/', f'src="{basepath}')
+
    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
    with open(dest_path, "w") as f:
       f.write(full_html)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(f"Generating pages from {dir_path_content} to {dest_dir_path} using {template_path}")
     
     for filename in os.listdir(dir_path_content):
@@ -65,9 +73,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(from_path):
             if filename.endswith(".md"):
                 dest_html_path = dest_path.replace(".md", ".html")
-                generate_page(from_path, template_path, dest_html_path)
+                generate_page(from_path, template_path, dest_html_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 
